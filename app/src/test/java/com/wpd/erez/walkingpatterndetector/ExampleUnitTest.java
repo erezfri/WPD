62f7b9f2ee2e377246d7239af4b28686d57f7dba
erez fridman <erezfri@gmail.com>
2016-12-31 21:33:09 +0200

    Env. update

--- app/src/test/java/com/wpd/erez/walkingpatterndetector/ExampleUnitTest.java
+++ app/src/test/java/com/wpd/erez/walkingpatterndetector/ExampleUnitTest.java
@@ -1,15 +1,16 @@
 package com.wpd.erez.walkingpatterndetector;
 
-import org.junit.Test;
+//import org.junit.Test;
 
-import static org.junit.Assert.*;
+//import static junit.framework.Assert.assertEquals;
+//import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-}
+//public class ExampleUnitTest {
+  //  @Test
+  //  public void addition_isCorrect() throws Exception {
+  //      assertEquals(4, 2 + 2);
+ //   }
+//}
